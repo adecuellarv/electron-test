32818821f1f6a7c53e4306801a92c116a28b9a60
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2022-08-05 01:50:00 -0400

    se cambia my-app a react

---
+++ my-app/src/components/home/home.js
@@ -0,0 +1,24 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { ipcRenderer } = require('electron');
+const { exec } = require('child_process');
+
+const Home = () => {
+    const test = () => {
+        exec('start chrome');
+    };
+
+    return (
+        <>
+            <h1>Home</h1>
+
+            <button onClick={test}>Click me</button>
+        </>
+    )
+};
+
+//export default Home;
+
+if (document.getElementById('home')) {
+    ReactDOM.render(<Home />, document.getElementById('home'));
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -30,10 +30,10 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'pages/home.html'));
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 const createSecondWindow = () => {
