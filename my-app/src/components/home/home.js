dd6681ce5fb117a9db975237be9dc1619722b53a
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2022-08-20 01:03:37 -0400

    diseño parte 1 de home

--- my-app/src/components/home/home.js
+++ my-app/src/components/home/home.js
@@ -1,22 +1,66 @@
 const React = require('react');
-const ReactDOM = require('react-dom');
-const { exec } = require('child_process');
-
+const ReactDOM = require('react-dom/client');
+//const bg  = require("./img/bg.png");
+//import * as bg from './img/bg.png';
+//const { exec } = require('child_process');
+const bgimage = document.getElementById('bgimage');
+const boxcenter = document.getElementById('boxcenter');
+const buttonstart = document.getElementById('buttonstart');
 const Home = () => {
+    const height = window.innerHeight;
     const test = () => {
         //exec('start chrome');
         //alert('amonos');
     };
-
+    console.log('height', height)
     return (
-        <>
-            <h1>Home</h1>
+        <div className="home-container"
+            style={{
+                backgroundImage: `linear-gradient(45deg,rgba(0, 0, 0, 0.30),rgba(0, 0, 0, 0.30) ), url(${bgimage?.src})`,
+                backgroundPosition: '50% 50%',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                position: 'relative'
+            }}
+        >
+            <div
+                style={{
+                    backgroundImage: `url(${boxcenter?.src})`,
+                    backgroundPosition: '50% 50%',
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    position: 'relative',
+                    width: '50%',
+                    margin: '0 auto',
+                    height: 'calc(100vh - 100px)',
+                    paddingTop: 100
+                }}
+            >
+                <div
+                    style={{
+                        position: 'absolute',
+                        width: '20%',
+                        left: '40%',
+                        //margin: '0 auto',
+                        bottom: '0px'
+                    }}
+                >
+                    <a href="assignment.html">
+                        <img src={buttonstart?.src}
+                            style={{
+                                width: '100%'
+                            }}
+                        />
+                    </a>
+                </div>
+            </div>
 
-            <a href="assignment.html">Comenzar</a>
-        </>
+        </div>
     )
 };
 
+
 if (document.getElementById('home')) {
-    ReactDOM.render(<Home />, document.getElementById('home'));
+    const root = ReactDOM.createRoot(document.getElementById('home'));
+    root.render(<Home />);
 }
